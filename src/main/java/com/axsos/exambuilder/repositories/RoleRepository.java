c4ae09f34f5241f3355faaf2f71bcf62a75913c3
NoorZak <n.zakarneh1@student.aaup.edu>
2021-01-20 10:09:17 +0200

    add spring security

---
+++ src/main/java/com/axsos/exambuilder/repositories/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.axsos.exambuilder.repositories;
+
+import com.axsos.exambuilder.models.Role;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    List<Role> findAll();
+
+    List<Role> findByName(String name);
+}
---
+++ src/main/java/com/axsos/exambuilder/services/UserService.java
@@ -0,0 +1,41 @@
+package com.axsos.exambuilder.services;
+
+
+import com.axsos.exambuilder.models.User;
+import com.axsos.exambuilder.repositories.RoleRepository;
+import com.axsos.exambuilder.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder)     {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+    // 1
+    public void saveWithUserRole(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(roleRepository.findByName("ROLE_USER"));
+        userRepository.save(user);
+    }
+
+    // 2
+    public void saveUserWithAdminRole(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(roleRepository.findByName("ROLE_ADMIN"));
+        userRepository.save(user);
+    }
+
+    // 3
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
