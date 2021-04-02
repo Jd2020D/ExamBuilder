972ddd7987cf16b33311d040de3b1aed205a3244
Jd2020D <jed19of@gmail.com>
2021-04-02 21:41:54 +0300

    edit class name

--- src/main/java/com/axsos/exambuilder/ExamBuilderApplication.java
+++ src/main/java/com/axsos/exambuilder/ExamBuilderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExambuilderApplication {
+public class ExamBuilderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExambuilderApplication.class, args);
+		SpringApplication.run(ExamBuilderApplication.class, args);
 	}
 
 }
